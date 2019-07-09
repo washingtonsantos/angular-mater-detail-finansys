dd9cafaf514e9abb2bd95dcaeeb300c448b7a279
washingtonsantos <washingtonsilvasan@gmail.com>
2019-07-09 14:58:18 -0300

    intercepting http requests with angular-in-memory-web-api

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api'; // Remover quando for usar o backend externo
+import { InMemoryDatabase } from "./in-memory-database"; // Remover quando for usar o backend externo
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule, 
+    HttpClientInMemoryWebApiModule.forRoot(InMemoryDatabase) // Remover quando for usar o backend externo
   ],
   providers: [],
   bootstrap: [AppComponent]
